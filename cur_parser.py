3e7b01e1410061ad237faa449b6c3fa6b833ca81
Kamil Babayev <kamilyunis@gmail.com>
2016-09-27 13:40:41 +0400

    added new script cur_passer, more easier way to parse

---
+++ cur_parser.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# This script will connect to central bank site and
+# get new  currency values for today
+
+import re
+from bs4 import BeautifulSoup
+import urllib.request
+from time import gmtime, strftime
+
+url = 'http://cbar.az'
+url_read = urllib.request.urlopen(url)
+soup = BeautifulSoup(url_read, 'lxml')
+time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+with open('note.txt', 'at') as f:
+    f.write(time)
+    f.write('\n')
+
+result  = soup.find_all('span', class_="item item_4")
+
+usd,eur,rub = result[0].text,result[1].text,result[2].text
+usd = "1 USD: " +  " " + usd
+eur = "1 EUR: " +  " " + eur
+rub = "1 RUB: " +  " " + rub
+cur_list = [usd,eur,rub]
+for cur in cur_list:
+    print(cur)
+
+with open('note.txt', 'at') as f:
+    for  cur in cur_list:
+        f.write(cur)
+        f.write('\n')
+    f.write(20*'-')
+    f.write("\n")
+
+print("\n")
+print('see note.txt file for old days')
