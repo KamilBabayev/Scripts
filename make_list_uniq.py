7f3b9cf18dba42eb7ac71f2e8fb92b5f7ff1d724
Kamil Babayev <kamilyunis@gmail.com>
2016-10-04 10:06:34 +0400

    uniq non uniq list maker

--- make_list_uniq.py
+++ make_list_uniq.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
-a = [2,3,4,55,3,100,100,33,44,55,100,3,4,4,4,4,4]
-print(a)
+a = [2,3,4,55,3,100,100,99,333,22,2,2]
+print('original list: ',a)
 for i in range(len(a)):
 	b = a[1+i:]
-	for x  in b:
+#	print(b)
+	for x in b:
 		if a[i] == x:
-			print(a[i])
 			a.remove(a[i])
-print(a)
+print('uniques: ',a)
+
+a = [2,3,4,55,3,100,100]
+c=[]
+for i in range(len(a)):
+    b = a[1+i:]
+#    print(b)
+    for x in b:
+        if a[i] == x:
+            c.append(a[i])
+print("Non uniques: ", c*2)
