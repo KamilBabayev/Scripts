4c43e320a8d5018a1740b73d79bae5f4cc18b185
Kamil Babayev <kamil.babayev@atltech.az>
2017-05-10 18:34:47 +0400

    email and rnadon feature added

--- selenium_unify_wifi_controller.py
+++ selenium_unify_wifi_controller.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 import time
+import smtplib
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from random import randint
+
+wifipass = ''
+for i in range(10):
+     wifipass = wifipass + str(randint(0,9))
+print(wifipass)
+
+
 firefoxProfile = webdriver.FirefoxProfile()
 firefoxProfile.set_preference('permissions.default.stylesheet', 2)
-firefoxProfile.set_preference('permissions.default.image', 2)
+#firefoxProfile.set_preference('permissions.default.image', 2)
 firefoxProfile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so','false')
 firefoxProfile.set_preference("http.response.timeout", 10)
 firefoxProfile.set_preference("dom.max_script_run_time", 10)
 driver = webdriver.Firefox()
-driver.get("https://unifi_controller_ip:8443")
+driver.get("https://unify_controller_ip:8443")
 time.sleep(1)
 username=driver.find_element_by_name("username")
-username.send_keys("username")
+username.send_keys("controller_username")
 time.sleep(1)
 password=driver.find_element_by_name("password")
-password.send_keys("password")
+password.send_keys("controller_password")
 time.sleep(1)
 login=driver.find_element_by_id("loginButton")
 login.click()
@@ -26,7 +35,8 @@ key = driver.find_element_by_name("wirelessNetworkWpaPskKey")
 time.sleep(1)
 key.clear()
 time.sleep(1)
-key.send_keys("wifi_password")
+#key.send_keys("8760251372")
+key.send_keys(wifipass)
 time.sleep(1)
 save=driver.find_element_by_class_name("busyToggle__body")
 time.sleep(1)
@@ -40,4 +50,23 @@ li:nth-child(1) > a:nth-child(3) > span:nth-child(2)")
 submenu.click()
 time.sleep(1)
 driver.close()
+
+
+
+aduser = 'domain\ad_user'
+adpass = 'ad_pass'
+FROM = 'fname.sname@domain.az'
+TO = ['useruser@mail.ru']
+SUBJECT = 'WIFI PASSWORD'
+TEXT = 'wifi password:   {}'.format(wifipass)
+mailserver = 'mail.domain.az'
+
+# Prepare actual message
+message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+conn = smtplib.SMTP(mailserver, 587)
+conn.starttls()
+conn.login(aduser, adpass)
+conn.sendmail(FROM,TO,message)
 
